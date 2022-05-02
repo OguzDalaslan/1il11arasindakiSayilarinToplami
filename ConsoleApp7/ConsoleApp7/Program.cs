62ce45f96587d2a5f508c106c7daa978146f8d8b
OguzDalaslan <104154904+OguzDalaslan@users.noreply.github.com>
2022-05-02 19:33:10 +0300

    1il11arasindakiSayilarinToplami

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 ile 10(dahil) arasındaki sayıların toplamı
+            int i;
+            int toplam = 0; // toplamada 0 etkisiz eleman
+            for (i = 1; i <= 10; i++)
+            {
+                toplam = toplam + i;
+            }
+            Console.WriteLine("Toplam:"+toplam);
+            Console.ReadKey();
+        }
+    }
+}
